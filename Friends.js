5a143915f7e6601523fa7425f73079ca6423c1ff
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2021-07-21 15:18:13 -0400

    New Files for Home Page

---
+++ Friends.js
@@ -0,0 +1,19 @@
+//Friends Page
+import React from 'react';
+import { StyleSheet, Text, Image, SafeAreaView } from 'react-native';
+
+export default function Friends() {
+  return (
+    <SafeAreaView style={{
+      backgroundColor: black,
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: "space-around",
+    }}>
+    </SafeAreaView>
+  );
+}
+
+
+
+
