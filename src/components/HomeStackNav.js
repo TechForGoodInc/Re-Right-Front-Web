745c6c5ade001c2cc3dcc8ddbb71e81f0ba252a3
nprate2 <42984229+nprate2@users.noreply.github.com>
2021-07-22 13:11:33 -0500

    Fixed hamburger functionality

--- src/components/HomeStackNav.js
+++ src/components/HomeStackNav.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 
-import colors from '../../config/colors';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
 export default function HomeStackNav({navigation}) {
+    const handleHamburgerPress = () => {
+        navigation.openDrawer();
+    }
     return (
         <Stack.Navigator initialRoute="Home" >
             <Stack.Screen name="Home" component={HomeScreen}
                 options={{title: "W E L C O M E", headerLeft: () => (
-                    <TouchableOpacity activeOpacity = { .5 } onPress={ navigation.openDrawer() }>
+                    <TouchableOpacity activeOpacity = { .5 } onPress={ handleHamburgerPress }>
                         <Image source={require('../../assets/HMIcon.png')} style = {styles.menuicon} />
                     </TouchableOpacity>
                     )}} />
