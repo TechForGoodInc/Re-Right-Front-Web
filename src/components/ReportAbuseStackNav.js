ea4ade5be054a8a110761ec4888543f4f589baa6
abhinavgupta <abhinav81003@gmail.com>
2021-07-26 22:55:01 +0530

    Report Abuse Screens Folder set up, fixed errors for navigation

--- src/components/ReportAbuseStackNav.js
+++ src/components/ReportAbuseStackNav.js
@@ -3,12 +3,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import ReportAbuseScreen1 from '../screens/ReportAbuseScreen1';
-import RAScreen2 from '../screens/ReportAbuseScreen2';
-// import ReportRecapArticle from '../screens/ReportAbuseRecapArticle';
-// import ReportRecapQuiz from '../screens/ReportAbuseRecapQuiz';
-// import ReportRecapRightsScreen from '../screens/ReportAbuseRecapRightsScreen';
-import RecapRightsScreen1 from '../screens/RecapRightsScreens/RecapRightsScreen1';
+import ReportAbuseScreen1 from '../screens/ReportAbuseScreens/ReportAbuseScreen1';
+import RAScreen2 from '../screens/ReportAbuseScreens/ReportAbuseScreen2';
+import ReportRecapArticle from '../screens/ReportAbuseScreens/ReportAbuseRecapArticle';
+import ReportRecapQuiz from '../screens/ReportAbuseScreens/ReportAbuseRecapQuiz';
+import ReportRecapRightsScreen from '../screens/ReportAbuseScreens/ReportAbuseRecapRightsScreen';
+
 
 const Stack = createStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -26,7 +26,9 @@ export default function ReportAbuseStackNav({navigation}) {
                     </TouchableOpacity>
                     )}} />
             <Stack.Screen name="Incident Info" component={RAScreen2} />
-            <Stack.Screen name="RecapRightsScreen" component={RecapRightsScreen1} />
+            <Stack.Screen name="Recap Rights" component={ReportRecapRightsScreen} />
+            <Stack.Screen name="Report RecapArticle" component={ReportRecapArticle} options = { ({route}) => ({title: route.params.title})} />  
+            <Stack.Screen name="Report Human Rights Quiz" component={ReportRecapQuiz} />   
         </Stack.Navigator>
     )
 }
