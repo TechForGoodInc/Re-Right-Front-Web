12540b96257dbc6ad0d03b4775591ba9ffeeb661
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2021-07-25 23:04:42 -0400

    Copies of RecapRights so no stack overlap

---
+++ src/screens/ReportAbuseRecapQuiz.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    View,
+     Text,
+} from "react-native";
+
+export default function ReportAbuseRecapQuiz() {
+    return (
+        <View>
+            <Text>
+                Test your Knowledge!
+            </Text>
+        </View>
+    )
+}
