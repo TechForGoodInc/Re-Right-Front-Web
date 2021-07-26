3f0acaccd077c9bccbac4df3c8234b00263f7d93
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2021-07-25 22:26:37 -0400

    Updating the aethetics

--- src/screens/ReportAbuseScreen2.js
+++ src/screens/ReportAbuseScreen2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Button, TextInput, Alert} from 'react-native';
+import {SafeAreaView, StyleSheet, Button, TextInput, Text} from 'react-native';
 
 export default function ReportAbuseScreen2({ navigation }) {
     return (
@@ -8,33 +8,61 @@ export default function ReportAbuseScreen2({ navigation }) {
         flexDirection: 'column',
         justifyContent: 'space-around',
         
-            }}>        
+            }}>
+            <Text
+                style = {styles.pretext}
+            >
+                Human Right Violated
+            </Text>        
             <TextInput
             style = {styles.textinput}
-            placeholder="Enter the Human Right Violated"
+            placeholder=" Enter the Human Right Violated"
             multiline={true} 
             >
             </TextInput>
+
+
             <Button
                 title="Don't Know?"
                 color = "dodgerblue"
                 onPress ={() => navigation.navigate('RecapRightsScreen')}
             />
+
+
+            <Text
+                style = {styles.pretext}
+            >
+                Location
+            </Text>     
             <TextInput
             style = {styles.textinput}
-            placeholder="Location of Violation"
+            placeholder=" Enter the Location of the Violation"
             multiline={true} 
             >
             </TextInput>
+
+
+            <Text
+                style = {styles.pretext}
+            >
+                Date of Occurence
+            </Text>   
             <TextInput
             style = {styles.textinput}
-            placeholder="Date of Violation" 
+            placeholder=" Enter the Date of the Violation" 
             multiline={true}
             >
             </TextInput>
+
+
+            <Text
+                style = {styles.pretext}
+            >
+                Offender?
+            </Text>   
             <TextInput
             style = {styles.textinput}
-            placeholder=" (Optional) Who committed the violation"
+            placeholder=" Describe the Person Who Committed the Crime"
             multiline={true}
  
             >
@@ -51,6 +79,9 @@ export default function ReportAbuseScreen2({ navigation }) {
     )
 }
 const styles = StyleSheet.create({
+    pretext:{
+        alignSelf: 'center',
+    },
     textinput: {
         backgroundColor: 'white',
         width: '80%',
