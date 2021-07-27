fa2cf0eb18bbd47722600092832e2051c04ffa56
nprate2 <42984229+nprate2@users.noreply.github.com>
2021-07-27 00:13:32 -0500

    Tweaked report abuse screen 1 styles

--- src/components/StackNavs/ReportAbuseStackNav.js
+++ src/components/StackNavs/ReportAbuseStackNav.js
@@ -4,14 +4,14 @@ import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import ReportAbuseScreen1 from '../../screens/ReportAbuseScreens/ReportAbuseScreen1';
-import RAScreen2 from '../../screens/ReportAbuseScreens/ReportAbuseScreen2';
+import ReportAbuseScreen2 from '../../screens/ReportAbuseScreens/ReportAbuseScreen2';
+
+import ReportRecapRightsScreen from '../../screens/ReportAbuseScreens/ReportAbuseRecapRightsScreen';
 import ReportRecapArticle from '../../screens/ReportAbuseScreens/ReportAbuseRecapArticle';
 import ReportRecapQuiz from '../../screens/ReportAbuseScreens/ReportAbuseRecapQuiz';
-import ReportRecapRightsScreen from '../../screens/ReportAbuseScreens/ReportAbuseRecapRightsScreen';
 
 
 const Stack = createStackNavigator();
-const BottomTabs = createBottomTabNavigator();
 
 export default function ReportAbuseStackNav({navigation}) {
     const handleHamburgerPress = () => {
@@ -25,7 +25,7 @@ export default function ReportAbuseStackNav({navigation}) {
                         <Image source={require('../../../assets/HMIcon.png')} style = {styles.menuicon} />
                     </TouchableOpacity>
                     )}} />
-            <Stack.Screen name="Incident Info" component={RAScreen2} />
+            <Stack.Screen name="Incident Info" component={ReportAbuseScreen2} />
             <Stack.Screen name="Recap Rights" component={ReportRecapRightsScreen} />
             <Stack.Screen name="Report RecapArticle" component={ReportRecapArticle} options = { ({route}) => ({title: route.params.title})} />  
             <Stack.Screen name="Report Human Rights Quiz" component={ReportRecapQuiz} />   
