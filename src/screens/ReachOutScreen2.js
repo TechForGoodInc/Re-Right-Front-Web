c0ea6e3609622dfe7d5b2698a88d40dd71e5b618
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2021-07-27 12:36:30 -0400

    Reach Out Page 2

---
+++ src/screens/ReachOutScreen2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, TextInput, Text, Pressable, Platform, Alert} from 'react-native';
+
+export default function ReportOutScreen2({ navigation }) {
+    return (
+        <SafeAreaView style={styles.screenBackground}>
+            <Text
+                style = {styles.pretext}
+            >
+                Reach Out Screen 2
+            </Text>        
+            
+        </SafeAreaView>
+
+    )
+}
