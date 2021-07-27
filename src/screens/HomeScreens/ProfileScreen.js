a05d470085b44a912fe0ab2ff71e1370ce8d3fbc
abhinavgupta <abhinav81003@gmail.com>
2021-07-27 21:39:26 +0530

    updated profile screen

--- src/screens/HomeScreens/ProfileScreen.js
+++ src/screens/HomeScreens/ProfileScreen.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import prompt from 'react-native-prompt-android';
-import { Button, Text, View, StyleSheet, Image, SafeAreaView, ScrollView, Pressable, TouchableOpacity, TextInput, Alert } from 'react-native'
+import { Platform, Button, Text, View, StyleSheet, Image, SafeAreaView, ScrollView, Pressable, TouchableOpacity, TextInput, Alert } from 'react-native'
 import { Prompt } from 'react-router-dom';
 import colors from '../../../config/colors';
 import SamplePost from './SamplePost';
@@ -12,11 +11,8 @@ var styles = StyleSheet.create({
         alignContent: 'center',
         textAlign: 'center',
         alignItems: 'center',
-        paddingTop: '5%'
-    },
-    profileImage: {
-        width: 100,
-        height: 110,
+        paddingTop: '0%',
+        marginHorizontal: 20,
     },
     bioAndTags: {
         paddingTop: '1%',
@@ -100,26 +96,23 @@ export default function ProfileScreen({navigation}) {
         setEditMode(!mode);
     }
     const editTags = () => {
-        return (
-          prompt(
-            'Select tags',
-            'Select the tags you wish to be displayed on your profile. These tags help you find friends, or Reach Out to people who have gone through the same experiences as you',
-            [
-             {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-             {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
-            ],
-            {
-                type: 'email-address',
-                cancelable: false,
-                
-                placeholder: 'placeholder'
-            }
-          )
-        );
+        if (Platform.OS === 'web'){
+            alert("Select/Deselect the tags you want")
+        }else {
+            Alert.alert(
+                "Edit Tags",
+                "Select/Deselect the tags you want",
+                [
+                    {
+                        text: "Go Back",
+                    }
+                ]
+            )
+        }
     }
     return (
         <ScrollView style={{
-            flex: 0,
+            flex: 1,
             padding: '2%',
             backgroundColor: "white",
             flexDirection: "column",
@@ -127,45 +120,47 @@ export default function ProfileScreen({navigation}) {
         }}>
             <View> 
             <TouchableOpacity  id= "editButton" onPress={() => {editing()} } title ="Edit "> 
-                <Image source= {require('../../../assets/editing.png')} style={{display: editMode? "none":'flex',color:editMode? "red":"blue", marginLeft: '88%',paddingTop:"5%", width: 30, height: 30}}/>
+                <Image source= {require('../../../assets/editing.png')} style={{display: editMode? "none":'flex',color:editMode? "red":"blue", marginLeft: '88%',paddingTop:"5%", width: (Platform.OS!=='web')? 35 :60 , height: (Platform.OS!=='web')? 35 :60}}/>
                 <View style= {{textAlign: "center" ,alignItems: 'center',backgroundColor: colors.light_grey, borderRadius: 10, marginLeft: '80%',marginBottom: '4%',marginRight: '3%'}}>
                             <Text style = {{display: editMode? "flex":'none',padding: '9%', color: colors.blue, fontWeight: '900'}}>Done</Text>
                         </View> 
             </TouchableOpacity>
             </View>
-            <View style = {styles.header}>
-                <Image style = {styles.profileImage}source={require('./../../../assets/man.png')}/> 
-                <Text style = {{display: editMode? 'none':'flex',fontWeight:'700',paddingTop:'1.5%',fontSize: 20, color: colors.black}}> {displayName} </Text>
-                <TextInput style = {{display: editMode? 'flex':'none', borderColor: colors.light_grey, borderWidth: 2, borderRadius: 10, marginTop: '1.5%',padding: '1%'}}
-                           defaultValue = {displayName}
-                           onChangeText = {name => setDisplayName(name)}
-                           onSubmitEditing = {editing}
-                />
-            </View>
-            <View style = {styles.bioAndTags}>
-                <Text style = {{fontWeight:'700',paddingBottom:'5%',fontSize: 18}} > Bio: </Text>
-                <Text style = {{display: editMode? 'none':'flex',fontSize: 17}}> {bio} </Text>
-                <TextInput style = {{display: editMode? 'flex':'none', borderWidth: 2, padding: '2%', borderRadius: 10,borderColor: colors.light_grey}}
-                           defaultValue = {bio}
-                           onChangeText = {bio => setBio(bio)}
-                           onSubmitEditing = {editing}
-                />
-                <View style = {{width: '100%', flexDirection: 'row'}}> 
-                    <Text style = {{fontWeight:'700',width: '77%', paddingVertical:5,fontSize: 18, alignSelf: 'flex-start'}} > Tags: </Text>
-                    <View style = {{fontSize: '13%', display: editMode? 'flex':'none', alignSelf: 'flex-end', alignContent: 'flex-end', textAlign: 'flex-end'}}> 
-                        <TouchableOpacity onPress={() => editTags()} backgroundColor = "red">  
-                        <View style= {{backgroundColor: colors.light_grey, borderRadius: 10, marginBottom: '4%', marginTop: '5%'}}>
-                            <Text style = {{padding: '2%', color: colors.red, fontWeight: '900'}}>Edit Tags</Text>
-                        </View> 
-                        </TouchableOpacity>
-                    </View>
+            <View style ={{flex:1, flexDirection:'row'}}>
+                <View style = {styles.header}>
+                    <Image style = {{ width: (Platform.OS!=='web') ? 100:130 , height: (Platform.OS!=='web') ? 110:150,}} source={require('./../../../assets/man.png')}/> 
+                    <Text style = {{display: editMode? 'none':'flex',fontWeight:'700',paddingTop:'1.5%',fontSize: 20, color: colors.black}}> {displayName} </Text>
+                    <TextInput style = {{display: editMode? 'flex':'none', borderColor: colors.light_grey, borderWidth: 2, borderRadius: 10, marginTop: '1.5%',padding: '1%'}}
+                            defaultValue = {displayName}
+                            onChangeText = {name => setDisplayName(name)}
+                            onSubmitEditing = {editing}
+                    />
                 </View>
-                <View style={styles.tagsGroup}>
-                    {tagList}
+                <View style = {styles.bioAndTags}>
+                    <Text style = {{fontWeight:'700',paddingBottom:'5%',fontSize: 18}} > Bio: </Text>
+                    <Text style = {{display: editMode? 'none':'flex',fontSize: 17}}> {bio} </Text>
+                    <TextInput style = {{display: editMode? 'flex':'none', borderWidth: 2, padding: '2%', borderRadius: 10,borderColor: colors.light_grey}}
+                            defaultValue = {bio}
+                            onChangeText = {bio => setBio(bio)}
+                            onSubmitEditing = {editing}
+                    />
+                    <View style = {{width: '100%', flexDirection: 'row'}}> 
+                        <Text style = {{fontWeight:'700',width: '77%', paddingVertical:5,fontSize: 18, alignSelf: 'flex-start'}} > Tags: </Text>
+                        <View style = {{fontSize: '13%', display: editMode? 'flex':'none', alignSelf: 'flex-end', alignContent: 'flex-end', textAlign: 'flex-end'}}> 
+                            <TouchableOpacity onPress={() => editTags()} backgroundColor = "red">  
+                            <View style= {{backgroundColor: colors.light_grey, borderRadius: 10, marginBottom: '4%', marginTop: '5%'}}>
+                                <Text style = {{padding: '2%', color: colors.red, fontWeight: '900'}}>Edit Tags</Text>
+                            </View> 
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={styles.tagsGroup}>
+                        {tagList}
+                    </View>
                 </View>
             </View>
             <View style = {styles.postBox}> 
-                <View style = {{flex:1,flexDirection: 'row', width: '100%', backgroundColor: colors.light_grey,paddingVertical: 2,marginVertical: 2, paddingHorizontal: '5%', alignContent: 'center',justifyContent: 'center'}}>
+                <View style = {{flex:1,flexDirection: 'row', width: '100%', backgroundColor: colors.light_grey,paddingVertical: 2,marginVertical: 2, paddingHorizontal: '1%', alignContent: 'center',justifyContent: 'center'}}>
                     <TouchableOpacity onPress={ () => setPostType('myPosts')}  style= {{ width:'50%',alignContent: 'center',textAlign:'center', justifyContent:'center', borderRightWidth: 1}}>
                         <View style= {{ width:'100%',alignItems:'center',  textAlign: 'center'}}> 
                             <Text> My Posts</Text>
