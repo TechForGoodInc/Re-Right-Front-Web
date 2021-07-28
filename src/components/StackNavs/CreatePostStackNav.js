99f425c2a3a234070679a19c53896b9e03926061
abhinavgupta <abhinav81003@gmail.com>
2021-07-28 19:39:27 +0530

    Added create post page, different types of posts set up :)

--- src/components/StackNavs/CreatePostStackNav.js
+++ src/components/StackNavs/CreatePostStackNav.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Image, TouchableOpacity } from 'react-native';
-
 import CreatePostScreen from '../../screens/HomeScreens/CreatePostScreen';
-
+import DevicePostScreen from '../../screens/HomeScreens/DevicePostScreen';
+import TextPostScreen from '../../screens/HomeScreens/TextPostScreen';
+import StylizedPostScreen from '../../screens/HomeScreens/StylizedPostScreen';
 const Stack = createStackNavigator();
 
 export default function CreatePostStackNav({navigation}) {
     const handleHamburgerPress = () => {
-        navigation.openDrawer();
+        navigation.navigate('Home');
     }
     return (
         <Stack.Navigator initialRoute="Create Post" >
             <Stack.Screen name="Create Post" component={CreatePostScreen}
-                options={{title: "Create Post", headerLeft: () => (
+                options={{title: "Create Post", headerRight: () => (
                     <TouchableOpacity activeOpacity = { .5 } onPress={ handleHamburgerPress }>
-                        <Image source={require('../../../assets/HMIcon.png')} style = {styles.menuicon} />
+                        <Image source={require('../../../assets/cross-round.png')} style = {styles.menuicon} />
                     </TouchableOpacity>
                     )}} />
+             <Stack.Screen name="Create Stylized Post" component={StylizedPostScreen}/>
+             <Stack.Screen name="Create Text-Only Post" component={TextPostScreen}/>
+             <Stack.Screen name="Choose From Device" component={DevicePostScreen}/>
         </Stack.Navigator>
     )
 }
 const styles = StyleSheet.create({
     menuicon: {
-      width: 50,
-      height: 50,
+      width: 30,
+      height: 30,
       resizeMode: "contain",
+      marginRight: 10
     },
   });
---
+++ src/screens/HomeScreens/DevicePostScreen.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Text } from "react-native";
+
+const DevicePostScreen = ({navigation}) => {
+    return ( 
+        <Text>Device Post</Text>
+     );
+}
+ 
+export default DevicePostScreen;
