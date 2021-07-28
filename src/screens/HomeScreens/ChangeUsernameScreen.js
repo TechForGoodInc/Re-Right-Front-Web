b66cb8f5684da5aa1b05a8c436fb5b55205ad4df
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2021-07-28 15:32:26 -0400

    Added Screen for changing username

---
+++ src/screens/HomeScreens/ChangeUsernameScreen.js
@@ -0,0 +1,10 @@
+//Chat Page
+import React from 'react';
+import { Text } from 'react-native';
+
+
+export default function ChangeUsernameScreen() {
+    return (
+        <Text>Change Username Screen</Text>
+    );
+}
