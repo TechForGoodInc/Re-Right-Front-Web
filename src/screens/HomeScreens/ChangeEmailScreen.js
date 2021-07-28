1b403635dc7199c7e099346a555fc53026a65cf7
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2021-07-28 17:27:06 -0400

    Change email screen fixed and updated

--- src/screens/HomeScreens/ChangeEmailScreen.js
+++ src/screens/HomeScreens/ChangeEmailScreen.js
@@ -1,166 +1,169 @@
-import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
-import React from 'react';
-import { Platform, View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import colors from '../../../config/colors';
+import { Platform, Form, Pressable, TouchableWithoutFeedback,Keyboard, View, StyleSheet, Text, Button, SafeAreaView, TextInput, KeyboardAvoidingView, Alert } from 'react-native';
+import GetSignUp1Style from '../styles/SignUp1Css';
+import KeyboardViewStyles from '../styles/KeyboardViewStyles';
+import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
 
-export default function ChangeUsernameScreen() {
-    //getting the dimensions and the orientation
-    const { landscape, portrait } = useDeviceOrientation();
+export default function SignupScreen1() {
+    
+    const { landscape } = useDeviceOrientation();
     const {width, height} = useDimensions().window;
+
+    //styles are in a sperate folder 
+    const [styles,setStyles] = useState(StyleSheet.create( 
+        GetSignUp1Style(landscape, width, height, colors) 
+    ));
+
+    if (landscape || width > height ){
+        () => {
+        setStyles(StyleSheet.create( 
+            GetSignUp1Style(landscape, width, height, colors) 
+        ))
+    }} 
+
+    //setting up the user input variables
+    const [email, setEmail] = useState('');
+    const [confirmEmail, setConfirmEmail] = useState('')
+    const [password, confirmPassword] = useState('');
+
+    //checking if user exists and showing an error if it does
+    const handleEmailChecks = (email) => {
+        const isEmailInUse = false;
+        //query the database to find the email, if it exists set isEmailInUse to true
+        return isEmailInUse;
+    }
+
+    const handleContinuePress = () => {
+        //checking for empty fields
+        const emailIsInUse = handleEmailChecks(email);
+        if (email==="" || confirmEmail =="" || password==="" ) {
+            if (Platform.OS === 'web'){
+                alert("Kindly fill all the fields to continue")
+            }else {
+                Alert.alert(
+                    "All fields are compulsory",
+                    "Kindly fill all the fields to continue",
+                    [
+                        {
+                            text: "Go Back",
+                            onPress: () => console.log(email +confirmEmail + password)
+                        }
+                    ]
+                )
+            }
+        } else if (emailIsInUse) {
+            if (Platform.OS === 'web'){
+                alert("An account is already registered with the same email address")
+            }else {
+                Alert.alert(
+                    "Email Already In Use",
+                    "An account is already registered with the same email address",
+                    [
+                        {
+                            text: "Enter Email Again",
+                            onPress: () => console.log('Used Email')
+                        }
+                    ]
+                )
+            }
+        }else if (email != confirmEmail) {
+            if (Platform.OS === 'web'){
+                alert("An account is already registered with the same email address")
+            }else {
+                Alert.alert(
+                    "Emails do not match",
+                    "Please make sure that the two email fields match",
+                    [
+                        {
+                            text: "Enter Email Again",
+                            onPress: () => console.log('Used Email')
+                        }
+                    ]
+                )
+            }
+        }else {
+            alert("Email Successfully Updated");
+        }
+    }
     
-    //styles are here
-    const styles = StyleSheet.create({
-
-        background: {
-            flex: 1,
-            backgroundColor: colors.white,
-            flexDirection: 'column',
-        },
-       
-         header: {
-             flex: 0.2,
-             alignItems: 'center',
-             justifyContent: 'center',
-         },
-       
-         inputbar: {
-             flex: 1,
-             justifyContent: 'space-around',
-             
-             ...Platform.select({
-                web: {
-                    marginLeft: '30%',
-                    marginRight: '30%',
-                },
-                default: {
-                    marginLeft: '5%',
-                    marginRight: '5%',
-                },
-             }),
-         },
-       
-         button: {
-             justifyContent: 'flex-end',
-             alignItems: 'center',
-             flex: 0.5,
-             bottom: '20%',
-             color: colors.blue, 
-             
-         },
-       
-         logo: {
-            resizeMode: 'contain', 
-            ...Platform.select({
-                web: {
-                  width: '25%',
-                  height: '75%',
-                },
-                default: {
-                  width: (landscape || width > height) ? '100%' : '55%',
-                  height: (landscape || width > height) ? '100%': '50%',
-                }
-            })
-         },
-       
-         inputFields: {
-             height: '18%',
-             margin: '1%',
-             width: '90%',
-             borderWidth: 2,
-             borderColor: colors.light_grey,
-             alignSelf: 'center',
-             fontSize: 16, 
-             borderRadius: 10,
-             padding: 20,
-         },
-      
-        
-          buttonContainer: {
-            flexDirection: 'row',
-          },
-      
-          continueButton: {
-            width: "100%",
-            height: 70,
-            justifyContent: "center",
-            alignItems: "center",
-          },
-      
-          buttonText: {
-            fontSize: 20,
-            lineHeight: 21,
-            fontWeight: 'bold',
-            letterSpacing: 0.25,
-            color: 'white',
-          },
-       
-        headertext: {
-            fontWeight: '700',
-            fontSize: 25, 
-            color: colors.blue,
-            marginBottom: '4%',
-          },
-      
-          linkView: {
-              alignItems: 'center',
-              bottom: '10%',
-              width: '100%'
-          },
-          linkText: {
-              color: colors.blue,
-              fontSize: 18,
-              marginBottom: '2%',
-          }
-       
-      });
-  const [email, onChangeUsername] = React.useState(null);     
-  return (
+    return (
+        <KeyboardAvoidingView style={styles.screenBackground}>
+            {/*  Wrapping the code with TouchableWithoutFeedback dismisses the keyboard when the user taps out of the input box*/}
+            <TouchableWithoutFeedback onPress={ Keyboard.dismiss}>
+                <SafeAreaView style = { styles.container} > 
+                    <View style = {styles.header}>
+                        <Text 
+                            style = {styles.headerTitle}>
+                            Change Email
+                        </Text>
+                    </View>
+                    <View styles = { styles.details }>
+                        <View>
+                            <Text 
+                                style={styles.label}>
+                                Enter your newly desired Email: 
+                            </Text>
+                            <TextInput 
+                                style={styles.inputText} 
+                                onChangeText = { email => setEmail(email) }
+                                defaultValue = {email}
+                                enablesReturnKeyAutomatically = {true}
+                                keyboardType = 'email-address'     // Works on all platforms
+                                textContentType = 'emailAddress'  // Only for iOS
+                                autoCompleteType='email'         // Only for Android
+                                autoCapitalize='none'           // Works on all platforms
+                                returnKeyType="next"
+                                // onSubmitEditing={() => { this.secondTextInput.focus() }}
+                                placeholder="New Email" />
+                        </View>
+                        <View>
+                            <Text 
+                                style={styles.label} >
+                                Confirm your new Email: 
+                            </Text>
+                            <TextInput 
+                                style={styles.inputText} 
+                                onChangeText = { email => setConfirmEmail(email) }
+                                defaultValue = {confirmEmail}
+                                enablesReturnKeyAutomatically = {true}
+                                keyboardType = 'email-address'     // Works on all platforms
+                                textContentType = 'emailAddress'  // Only for iOS
+                                autoCompleteType='email'         // Only for Android
+                                autoCapitalize='none'           // Works on all platforms
+                                returnKeyType="next"
+                                placeholder="Confirm new Email" />
+                        </View>
+                    </View>
+                    <View styles = { styles.password }>
+                        <View>
+                            <Text 
+                                style={styles.label}>
+                                Enter Password to ensure changes: 
+                            </Text>
+                            <TextInput 
+                                enablesReturnKeyAutomatically = {true}
+                                secureTextEntry={true} 
+                                onChangeText = { email => confirmPassword(email) }
+                                returnKeyType="next"
+                                defaultValue = {password}
+                                textContentType = "newPassword" 
+                                autoCompleteType='password' 
+                                style={styles.inputText} 
+                                placeholder="Password" 
+                                autoCapitalize='none'/>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </TouchableWithoutFeedback>
+            <Pressable style={({pressed}) => [{
+                backgroundColor: pressed ? colors.grey : colors.blue,},
+                styles.continueButton,]}
+                onPress={() => handleContinuePress()}>
+                <Text style={styles.buttonText}>Continue</Text>
+            </Pressable>
+        </KeyboardAvoidingView>
+    );
     
-    <View style = {styles.background}>
-        <View style={styles.header}>
-        </View>
-
-        <View style = {styles.inputbar}>
-            <Text style = {styles.headertext}> Change Your Email: </Text>
-            
-            <Text style={styles.labels}> Enter your new desired Email:  </Text>
-            <TextInput
-             style={styles.inputFields}
-             placeholder = ' New Email'
-             />
-             <Text style={styles.labels}> Confirm your new desired Email:  </Text>
-            <TextInput
-             style={styles.inputFields}
-             onChangeText={onChangeUsername}
-             value={email}
-             placeholder = ' Confirm New Email'
-             textContentType='username'
-             autoCompleteType='email' 
-             autoCapitalize='none'
-             />
-            <Text style={styles.labels} > Enter your password to ensure the change is saved:  </Text>
-            <TextInput 
-             style={styles.inputFields}
-             secureTextEntry={true} 
-             autoCompleteType='password' 
-             autoCapitalize='none'
-             textContentType = 'password'
-             placeholder = ' Password'/>
-        </View>
-
-        <View style = {styles.button}>
-            <Button
-                title="Update Email"
-                accessibilityLabel="Learn more about this purple button"
-                onPress={() =>alert("Email Successfully Updated")}
-            />
-        </View>
-    </View>
-  );
 }
- 
-
-
-
-
 
