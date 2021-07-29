7fed2e049fac4d20c8268e23cd96c40c30b30cce
nprate2 <42984229+nprate2@users.noreply.github.com>
2021-07-28 21:18:56 -0500

    Added drawer icons

--- src/components/HomeDrawerNav.js
+++ src/components/HomeDrawerNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StyleSheet } from 'react-native';
+import { AntDesign, MaterialIcons, FontAwesome, MaterialCommunityIcons, FontAwesome5 } from '@expo/vector-icons';
 
 import HomeTabNav from './TabNavs/HomeTabNav';
 import RecapRightsTabNav from './TabNavs/RecapRightsTabNav';
@@ -42,13 +43,27 @@ export default function HomeDrawerNav({navigation}) {
                 
             }}>
 
-            <Drawer.Screen name="Home" component={HomeTabNav}/>
-            <Drawer.Screen name="Recap Rights" component={RecapRightsTabNav}/>
-            <Drawer.Screen name="Report Abuse" component={ReportAbuseStackNav}/>
-            <Drawer.Screen name="Reach Out" component={ReachOutStackNav}/>
-            <Drawer.Screen name="Research Analytics" component={ResearchStackNav}/>
-            <Drawer.Screen name="Profile" component={ProfileStackNav}/>
-            <Drawer.Screen name="Log Out" component={LoggedOutScreen} labelStyle={styles.logoutLabelStyle}/>
+            <Drawer.Screen name="Home" component={HomeTabNav} options={{
+                drawerIcon: () => <AntDesign name="home" size={24} color="black" />
+            }}/>
+            <Drawer.Screen name="Recap Rights" component={RecapRightsTabNav} options={{
+                drawerIcon: () => <FontAwesome name="book" size={24} color="black" />
+            }}/>
+            <Drawer.Screen name="Report Abuse" component={ReportAbuseStackNav} options={{
+                drawerIcon: () => <MaterialIcons name="report" size={24} color="black" />
+            }}/>
+            <Drawer.Screen name="Reach Out" component={ReachOutStackNav} options={{
+                drawerIcon: () => <AntDesign name="wechat" size={24} color="black" />
+            }}/>
+            <Drawer.Screen name="Research Analytics" component={ResearchStackNav} options={{
+                drawerIcon: () => <MaterialCommunityIcons name="brain" size={24} color="black" />
+            }}/>
+            <Drawer.Screen name="Profile" component={ProfileStackNav} options={{
+                drawerIcon: () => <FontAwesome5 name="user" size={24} color="black" />
+            }}/>
+            <Drawer.Screen name="Log Out" component={LoggedOutScreen} labelStyle={styles.logoutLabelStyle} options={{
+                drawerIcon: () => <MaterialCommunityIcons name="exit-run" size={24} color="black" />
+            }}/>
         </Drawer.Navigator>
     )
 }
