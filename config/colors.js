a21d88adceca9de035277ea09d54238191af85f1
nprate2 <42984229+nprate2@users.noreply.github.com>
2021-07-30 14:11:30 -0500

    Added color palette to colors.js

--- config/colors.js
+++ config/colors.js
@@ -1,22 +1,25 @@
 export default {
-    primary: "#4ecdc4",
-    secondary: "#fc5c65",
-
-    black: "#000",
-    white: "#fff",
-
-    red: "tomato",
-
-    grey: "grey",
-    light_grey: "#D3D3D3",
-    lighter_grey : '#E0E0E0',
-
-    blue: "#428AF8",
-    light_blue: "#ADD8E6",
-
-    cream: "#FDFCDC",
-    dblue: "#0081A7",
+    // Palette start
+    primary: "#3987CE",
+    primary_dark: "#1E3473",
+    primary_light: "#73C7FF",
+    secondary: "#007F7F",
+    secondary_dark: "#144F4B",
+    accent1: "#FCD681",
+    accent2: "#EA8071",
 
     active_tab: "#ADD8E6",
     inactive_tab: "#428AF8",
+
+    black: "11111E", // Palette
+    white: "#fff",
+    red: "tomato",
+    grey: "grey",
+    light_grey: "#D3D3D3",
+    lighter_grey : '#E0E0E0',
+    blue: "#428AF8",
+    light_blue: "#ADD8E6",
+    cream: "#FDFCDC",
+    dblue: "#0081A7",
+
 }
