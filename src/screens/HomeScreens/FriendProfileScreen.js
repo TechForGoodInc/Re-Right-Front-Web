cecfe10ee1050db34d40462448e54eb4eabfe575
nprate2 <42984229+nprate2@users.noreply.github.com>
2021-08-02 12:59:13 -0500

    Tweaks

---
+++ src/screens/HomeScreens/FriendProfileScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function FriendProfileScreen() {
+    return (
+        <Text>Friend Profile Screen</Text>
+    )
+}
--- src/screens/HomeScreens/FriendsScreen.js
+++ src/screens/HomeScreens/FriendsScreen.js
@@ -1,14 +1,45 @@
 //Friends Page
 import React from 'react';
-import { StyleSheet, Text, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, TextInput, SafeAreaView, ScrollView } from 'react-native';
 
 import colors from '../../../config/colors';
-export default function FriendsScreen() {
+import SampleFriend from './SampleFriend';
+export default function FriendsScreen({navigation}) {
     return (
-        <Text>Friends Screen</Text>
+        <SafeAreaView style= {styles.container}>
+            <TextInput style={styles.textInput}
+                placeholder="Search">
+            </TextInput>
+            <ScrollView>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+                <SampleFriend/>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+        top: '1%',
+        paddingTop: '1%',
+        paddingBottom: '10%',
+        paddingLeft: '1%',
+    },
 
-
-
-
+    textInput: {
+        paddingTop: '1%',
+        paddingBottom: '1%',
+        paddingLeft: '1%',
+        backgroundColor: 'white',
+        width: '80%',
+        height: '5%',
+        alignSelf: 'center',
+    }
+})
