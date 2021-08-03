666344ede18f7de8e4ca9ae46be7bff10830b889
Nyun Ei Hlaing <nyuneihlaing1@gmail.com>
2021-08-03 15:22:32 -0700

    remove friend feature issue

--- src/screens/HomeScreens/FriendsScreen.js
+++ src/screens/HomeScreens/FriendsScreen.js
@@ -1,27 +1,27 @@
 //Friends Page
-import React from 'react';
-import { View,StyleSheet, Text, Image, FlatList, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import { View,StyleSheet, Text, Image, FlatList, TextInput, TouchableOpacity} from 'react-native';
 import '../../../config/global';
 import color from '../../../config/colors';
 import darkColors from '../../../config/darkColors';
 
 const data = [
-    { id: '1', title: 'Abhinav Gupta', mutual: '25' },
-    { id: '2', title: 'Nyun Ei Hlaing', mutual: '23' },
-    { id: '3', title: 'Nicolas Prate', mutual: '12' },
-    { id: '4', title: 'Nathan Cook', mutual: '30' },
-    { id: '5', title: 'Abhinav Gupta', mutual: '25' },
-    { id: '6', title: 'Nyun Ei Hlaing', mutual: '23' },
-    { id: '7', title: 'Nicolas Prate', mutual: '12' },
-    { id: '8', title: 'Nathan Cook', mutual: '30' },
-    { id: '9', title: 'Abhinav Gupta', mutual: '25' },
-    { id: '10', title: 'Nyun Ei Hlaing', mutual: '23' },
-    { id: '11', title: 'Nicolas Prate', mutual: '12' },
-    { id: '12', title: 'Nathan Cook', mutual: '30' },
-    { id: '13', title: 'Abhinav Gupta', mutual: '25' },
-    { id: '14', title: 'Nyun Ei Hlaing', mutual: '23' },
-    { id: '15', title: 'Nicolas Prate', mutual: '12' },
-    { id: '16', title: 'Nathan Cook', mutual: '30' },
+    { id: '0', title: 'Abhinav Gupta', mutual: '25' },
+    { id: '1', title: 'Nyun Ei Hlaing', mutual: '23' },
+    { id: '2', title: 'Nicolas Prate', mutual: '12' },
+    { id: '3', title: 'Nathan Cook', mutual: '30' },
+    { id: '4', title: 'Abhinav Gupta', mutual: '25' },
+    { id: '5', title: 'Nyun Ei Hlaing', mutual: '23' },
+    { id: '6', title: 'Nicolas Prate', mutual: '12' },
+    { id: '7', title: 'Nathan Cook', mutual: '30' },
+    { id: '8', title: 'Abhinav Gupta', mutual: '25' },
+    { id: '9', title: 'Nyun Ei Hlaing', mutual: '23' },
+    { id: '10', title: 'Nicolas Prate', mutual: '12' },
+    { id: '11', title: 'Nathan Cook', mutual: '30' },
+    { id: '12', title: 'Abhinav Gupta', mutual: '25' },
+    { id: '13', title: 'Nyun Ei Hlaing', mutual: '23' },
+    { id: '14', title: 'Nicolas Prate', mutual: '12' },
+    { id: '15', title: 'Nathan Cook', mutual: '30' },
 ]
 
 export default function FriendsScreen() {
@@ -82,7 +82,26 @@ export default function FriendsScreen() {
             marginLeft: '5%',
             alignSelf: 'center',
         },
+        removeButton : {
+            padding: 5,
+            margin: 20,
+            height: '60%',
+            width: '20%',
+            backgroundColor:colors.primary,
+            alignSelf: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            right: 0,
+        }
       });
+    const [list, setList] = useState(data);
+    const handleRemoveFriend = (id) => {
+        var newList = list;
+        newList.splice(id,1);
+        setList(newList);
+        console.log(JSON.stringify(list));
+        console.log(id);
+    }
     return (
         <View style={styles.container}>
                 <View style = {styles.searchBar}>
@@ -105,6 +124,12 @@ export default function FriendsScreen() {
                                 <Text style= {{fontSize:15, color: colors.text_general, fontWeight: '700' }}>{item.title}</Text>
                                 <Text style={{paddingTop: 5, color: colors.text_subtitle}}>{item.mutual} mutual friends</Text>
                             </View>
+                            <TouchableOpacity
+                                onPress={()=> handleRemoveFriend(item.id)}
+                                style={styles.removeButton}>
+                                <Text style = {{color: colors.white, fontSize: 10, fontWeight: 'bold'}}>Remove Friend</Text>
+                            </TouchableOpacity>
+
                         </View>
                     )}
                 />
